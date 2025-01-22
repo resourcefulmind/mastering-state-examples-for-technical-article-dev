0b37a2ea578231fd04a30f76091112914cdd91a7
resourcefulmind <resourcefulmindoso@gmail.com>
2025-01-22 12:50:39 +0100

    Your changes

---
+++ mastering-state-examples-for-technical-article-dev/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a simple React app</p>
+    </>
+  )
+}
+
+export default App
